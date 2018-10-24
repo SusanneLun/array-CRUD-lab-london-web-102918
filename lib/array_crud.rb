c69bb4978c1ac281673d9fbadd09951817ac1603
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 16:38:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [tip, tap, toll, cross]
+  array = ["tip", "tap", "toll", "cross"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << 
+  array << "bill"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
